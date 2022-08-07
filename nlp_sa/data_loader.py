3bf98aadbda8a7d850dccf17441fb38b04afee6a
puneet-jain159 <puneet.jain@databricks.com>
2022-08-07 19:29:34 +0100

    add data_loader

--- nlp_sa/data_loader.py
+++ nlp_sa/data_loader.py
@@ -1,5 +1,5 @@
 
-from datasets import load_dataset, load_metric
+from datasets import load_dataset, load_metric,Dataset
 from transformers.utils import logging
 
 logger = logging.get_logger(__name__)
@@ -12,8 +12,10 @@ class DataLoader:
 
     def __init__(
             self,
-            conf):
+            conf,
+            spark = None):
         self.conf = conf
+        self.spark = spark
         self._LoadDataSet()
 
     def _LoadDataSet(self):
@@ -24,8 +26,33 @@ class DataLoader:
         if self.conf.data_args.dataset_name:
             logger.debug(
                 f"Loading the dataset from hugging face dataset: {self.conf.data_args.dataset_name}")
-            self.raw_dataset = load_dataset(
-                self.conf.data_args.dataset_name,
-                self.conf.data_args.dataset_config_name,
-                cache_dir=self.conf.model_args.cache_dir,
-                use_auth_token=True if self.conf.model_args.use_auth_token else None)
+            if self.conf.training_args.do_train:
+                self.train = load_dataset(
+                    self.conf.data_args.dataset_name,
+                    self.conf.data_args.dataset_config_name,
+                    split = "train",
+                    cache_dir=self.conf.model_args.cache_dir,
+                    use_auth_token=True if self.conf.model_args.use_auth_token else None)
+            
+            if self.conf.training_args.do_eval:
+                self.test = load_dataset(
+                    self.conf.data_args.dataset_name,
+                    self.conf.data_args.dataset_config_name,
+                    split = "test",
+                    cache_dir=self.conf.model_args.cache_dir,
+                    use_auth_token=True if self.conf.model_args.use_auth_token else None)
+
+        elif self.conf.data_args.database_name:
+            logger.debug(
+                f"Loading the data from Database: {self.conf.data_args.database_name}")
+
+            if self.conf.data_args.train_table:
+                logger.debug(
+                    f"Loading the data from table: {self.conf.data_args.train_table}")
+                train = spark.read.table(f"{self.conf.data_args.database_name}.{self.conf.data_args.train_table}")
+                train = train.toPandas()
+                self.train = Dataset.from_pandas(train)
+
+
+
+
