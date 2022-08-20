8531001b5220ec223dfaa6390d3cd3e6c00c6df8
Rafael Pierre <rafael.pierre@databricks.com>
2022-08-20 14:50:34 +0200

    [wip] flake preprocess.py

--- nlp_sa/preprocess.py
+++ nlp_sa/preprocess.py
@@ -31,40 +31,35 @@ def _preprocess_function_multi_class(examples, conf, Dataset, Model):
         truncation=True
     )
 
-    # # Map labels to IDs (not necessary for GLUE tasks)
-    # if Model.model.config.label2id is not None and conf.data_args.feature_col in examples:
-    #     result[conf.data_args.feature_col] = [(Model.model.config.label2id [l] if l != -1 else -1)
-    #                        for l in examples[conf.data_args.feature_col]]
     return result
 
 
-def _preprocess_function_ner(examples, conf, Dataset, Model):
+def _preprocess_function_ner(examples, conf, dataset, model):
     """
     Function to Tokenize the data and perform any preprocessing required
     """
-    # Map that sends B-Xxx label to its I-Xxx counterpart
     b_to_i_label = []
-    for idx, label in enumerate(Dataset.label_list):
+    for idx, label in enumerate(dataset.label_list):
         if (
             str(label).startswith("B-")
-            and str(label).replace("B-", "I-") in Dataset.label_list
+            and str(label).replace("B-", "I-") in dataset.label_list
         ):
             b_to_i_label.append(
-                label_list.index(str(label).replace("B-", "I-"))
+                dataset.label_list.index(str(label).replace("B-", "I-"))
             )
         else:
             b_to_i_label.append(idx)
-
     padding = (
         "max_length" if conf.data_args.pad_to_max_length else False
     )
 
-    tokenized_inputs = Model.tokenizer(
+    """We use is_split_into_words because the texts in our dataset
+    are lists of words (with a label for each word)."""
+    tokenized_inputs = model.tokenizer(
         examples[conf.data_args.feature_col],
         padding=padding,
         truncation=True,
         max_length=conf.data_args.max_seq_length,
-        # We use this argument because the texts in our dataset are lists of words (with a label for each word).
         is_split_into_words=True,
     )
     labels = []
@@ -73,20 +68,22 @@ def _preprocess_function_ner(examples, conf, Dataset, Model):
         previous_word_idx = None
         label_ids = []
         for word_idx in word_ids:
-            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
-            # ignored in the loss function.
+            """Special tokens have a word id that is None.
+            We set the label to -100 so they are automatically
+            ignored in the loss function."""
             if word_idx is None:
                 label_ids.append(-100)
             # We set the label for the first token of each word.
             elif word_idx != previous_word_idx:
-                label_ids.append(Dataset.label_to_id[label[word_idx]])
-            # For the other tokens in a word, we set the label to either the current label or -100, depending on
-            # the label_all_tokens flag.
+                label_ids.append(dataset.label_to_id[label[word_idx]])
             else:
+                """For the other tokens in a word, we set the label
+                to either the current label or -100, depending on
+                the label_all_tokens flag."""
                 if conf.data_args.label_all_tokens:
                     label_ids.append(
                         b_to_i_label[
-                            Dataset.label_to_id[label[word_idx]]
+                            dataset.label_to_id[label[word_idx]]
                         ]
                     )
                 else:
